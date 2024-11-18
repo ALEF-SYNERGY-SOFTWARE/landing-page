22568af7e216475909df85a06a626a5370fa063c
Fábio Marins <fabiomarinspro@hotmail.com>
2024-11-17 23:43:00 -0300

    feat: update md:mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,22 +6,19 @@ export function Header() {
    <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
     <div className="flex-shrink-0">
      <a href="#" className="-m-1.5 p-1.5">
-      <img alt="" src={ImgAvatar} className="h-20 w-auto" />
+      <img alt="" src={ImgAvatar} className="h-12 w-auto md:h-20" />
      </a>
     </div>
-    <div className="flex">
-     <span className="hidden sm:block">
-      <button
-       type="button"
-       className="inline-flex items-center rounded-md px-5 py-3 text-lg font-semibold text-white bg-red-500 hover:bg-red-600"
-       onClick={() => {
-        window.location.href =
-         "https://invoice.gamecms.com.br/client/login?company_key=2RIDDxYMpar1FNgFrzjKBL4yRkCwp7GQ";
-       }}
-      >
-       Entrar
-      </button>
-     </span>
+    <div className="flex-shrink-0">
+     <button
+      type="button"
+      className="inline-flex items-center rounded-md px-3 py-2 text-sm md:px-5 md:py-3 md:text-lg font-semibold text-white bg-red-500 hover:bg-red-600"
+      onClick={() => {
+       window.open("https://invoice.gamecms.com.br/client/login?company_key=2RIDDxYMpar1FNgFrzjKBL4yRkCwp7GQ")
+      }}
+     >
+      Entrar
+     </button>
     </div>
    </nav>
   </header>
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,42 +3,42 @@ import ImgBanner from "/assets/banner-game-cms.png";
 export function MainContent() {
  return (
   <>
-   <div className="flex py-20">
+   <div className="flex py-2 px-2 md:py-20">
     <div className="mx-auto max-w-7xl lg:px-8">
      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
       <div className="items-center">
-       <div className="lg:max-w-lg">
-        <h2 className="text-red-500 font-serif">
+       <div className="lg:max-w-lg ">
+        <h2 className="text-red-500 font-serif text-xs md:text-lg">
          Deseja gerenciar seu game indie / privado de forma rápida e fácil?
         </h2>
-        <p className="mt-6 text-6xl text-white sm:text-5xl font-serif font-semibold">
+        <p className="mt-6 text-3xl md:text-6xl text-white font-serif font-semibold">
          Suba de nível!
         </p>
 
-        <p className="mt-2 mb-2 text-lg/8 text-white font-serif font-light">
+        <p className="mt-2 mb-2 md:text-lg/8 text-white font-serif font-light">
          Sistema de Loja e-commerce integrado com game;
         </p>
 
-        <p className="mt-2 mb-2 text-lg/8 text-white font-serif font-light">
+        <p className="mt-2 mb-2 md:text-lg/8 text-white font-serif font-light">
          Sistema de Guild com senha;
         </p>
 
-        <p className="mt-2 mb-2 text-lg/8 text-white font-serif font-light">
+        <p className="mt-2 mb-2 md:text-lg/8 text-white font-serif font-light">
          Sistema de notificação via discord;
         </p>
 
-        <p className="mt-2 mb-2 text-lg/8 text-white font-serif font-light">
+        <p className="mt-2 mb-2 md:text-lg/8 text-white font-serif font-light">
          Sistema de Atualização para launcher nativo e fácil de usar;
         </p>
 
-        <p className="mt-2 mb-2 text-lg/8 text-white font-serif font-light">
+        <p className="mt-2 mb-2 md:text-lg/8 text-white font-serif font-light">
          Tudo isso e muito mais!
         </p>
 
-        <span className="hidden sm:block">
+        <span>
          <button
           type="button"
-          className="mt-4  bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded "
+          className="mt-4  bg-green-500 hover:bg-green-700 text-white font-bold py-1 md:py-2 px-3 md:px-4 border border-green-700 rounded "
           onClick={() => {
            window.location.href =
             "https://api.whatsapp.com/send/?phone=5551991007356&text=ola+gostaria+de+contratar+a+gamecms&type=phone_number&app_absent=0";
@@ -55,7 +55,7 @@ export function MainContent() {
         src={ImgBanner}
         width={300}
         height={300}
-        className="w-[48rem] rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+        className="md:w-[48rem] mx-auto rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
        />
       </div>
      </div>
@@ -63,7 +63,7 @@ export function MainContent() {
    </div>
 
    <div className="mt-8 text-center">
-    <p className="text-lg text-red-500 font-serif font-semibold">
+    <p className="md:text-lg text-red-500 font-serif font-semibold">
      A melhor solução para seu Game
     </p>
     <p className="text-white text-2xl font-serif font-semibold mt-2">
@@ -74,7 +74,6 @@ export function MainContent() {
    <div className="mx-auto">
     <div className="flex justify-center">
      <iframe
-      className="flex justify-center"
       width="1024"
       height="768"
       src="https://player.vimeo.com/video/1025858770?h=33469dc7a2&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
@@ -86,11 +85,10 @@ export function MainContent() {
 
     <div className="flex justify-center">
      <button
-      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded mb-10 w-full max-w-5xl"
+      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-28 md:px-10 border border-red-700 rounded mb-10 md:w-full md:max-w-5xl"
       onClick={() => {
-       window.location.href =
-        "https://invoice.gamecms.com.br/client/subscriptions/VolejRejNm/purchase";
-      }}
+       window.open("https://invoice.gamecms.com.br/client/subscriptions/VolejRejNm/purchase")
+ }}
      >
       Comprar Agora
      </button>
